3028f1ad52a12d6d788d0a4cf51701e98f1078b5
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2023-05-12 19:30:06 +0300

    Implement qsort

---
+++ algorithms/qsort.py
@@ -0,0 +1,11 @@
+def qsort(arr: list):
+    if len(arr) < 2:
+        return arr
+    base = arr.pop(0)
+    smaller = [item for item in arr if item <= base]
+    greater = [item for item in arr if item > base]
+    return qsort(smaller) + [base] + qsort(greater)
+
+
+ls = [24, 15, -51, 153, 1, 1, 3413, 13, 13]
+print(qsort(ls))

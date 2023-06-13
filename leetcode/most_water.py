6df63eab78ee753d03ed0b2b9726df733853deb6
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2023-06-13 22:47:50 +0300

    Finish "Container With Most Water" task

--- leetcode/most_water.py
+++ leetcode/most_water.py
@@ -6,19 +6,30 @@ class Solution:
 
     @staticmethod
     def max_area(height: list[int]) -> int:
-        max_v = 0
-        size = len(height)
+        max_area = 0
         for main_index, main_item in enumerate(height):
             for minor_index, minor_item in enumerate(height[main_index+1:]):
                 h = min(main_item, minor_item)
                 w = minor_index + 1
-                v = h * w
-                # print(f'height = {h}, width = {w}')
-                # print(f'volume = {h * w}\n')
-                max_v = max(max_v, v)
-        return max_v
+                s = h * w
+                max_area = max(max_area, s)
+        return max_area
+
+    @staticmethod
+    def max_area_gs(height: list[int]) -> int:
+        max_s = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            current = (right - left) * min(height[left], height[right])
+            max_s = max(current, max_s)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_s
 
 
 test_height = [10, 8, 6, 2, 5, 4, 8, 10, 7]
-test_height2 = [1, 7, 4, 2]
-print(Solution.max_area(test_height))
+test_height2 = [2]
+print(Solution.max_area(test_height2))
